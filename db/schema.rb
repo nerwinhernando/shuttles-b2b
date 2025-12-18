c741387b54c8d9099db60d30a3a96a21534c8728
Nerwin Hernando <nerwinc.hernando@gmail.com>
2025-12-19 05:56:48 +0800

    setup users model

--- db/schema.rb
+++ db/schema.rb
@@ -31,14 +31,32 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_18_214849) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.bigint "account_id"
+    t.boolean "active", default: true
     t.datetime "created_at", null: false
+    t.datetime "current_sign_in_at"
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "last_sign_in_at"
+    t.string "last_sign_in_ip"
+    t.integer "loyalty_points", default: 0
+    t.string "phone"
+    t.string "player_type"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "role", default: "customer"
+    t.integer "sign_in_count", default: 0, null: false
+    t.string "skill_level"
     t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_users_on_account_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
+
+  add_foreign_key "users", "accounts"
 end
