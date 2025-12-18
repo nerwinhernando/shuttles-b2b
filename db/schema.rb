0d6cb2dad8566fc07bdf6c6fe64be2be1e17f828
Nerwin Hernando <nerwinc.hernando@gmail.com>
2025-12-19 05:47:50 +0800

    rails generate model Account name:string subdomain:string owner_name:string owner_email:string phone:string address:text city:string province:string plan:string active:boolean settings:json

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_18_214737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "accounts", force: :cascade do |t|
+    t.boolean "active"
+    t.text "address"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "owner_email"
+    t.string "owner_name"
+    t.string "phone"
+    t.string "plan"
+    t.string "province"
+    t.json "settings"
+    t.string "subdomain"
+    t.datetime "updated_at", null: false
+  end
 end
